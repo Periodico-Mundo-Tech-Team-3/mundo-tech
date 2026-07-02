f0cff540de56b8f19f532e003d141806adfbee0f
Rosa Naharro Vaillant <rosana50factoria@gmail.com>
2026-07-03 00:02:44 +0200

    feat: create model Article

---
+++ src/main/java/com/mundotech/newspaper/entity/Article.java
@@ -0,0 +1,56 @@
+package com.mundotech.newspaper.entity;
+
+import java.util.Date;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "articles")
+@Data
+@NoArgsConstructor
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private Date publishDate;
+
+    @Enumerated(EnumType.STRING)
+    private EstadoDocumento status;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user_id", nullable = true, referencedColumnName = "id")
+    private User user;
+}
+
+enum EstadoDocumento {
+    DRAFT,
+    IN_REVIEW,
+    PUBLISHED
+}
+
