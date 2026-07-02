8e591b47fde8277d968ccc902eaa9b0c04c0837d
Rosa Naharro Vaillant <rosana50factoria@gmail.com>
2026-07-03 01:53:17 +0200

    feat:create repsitory, service, controler y test en postman

---
+++ src/main/java/com/mundotech/newspaper/service/ArticleService.java
@@ -0,0 +1,11 @@
+package com.mundotech.newspaper.service;
+
+import com.mundotech.newspaper.entity.Article;
+
+
+public interface ArticleService {
+     
+
+    public Article createArticle(Article article, int userId);
+
+}
