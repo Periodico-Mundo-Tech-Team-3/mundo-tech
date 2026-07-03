0d08af20cfc923f2f7e355121cc5ebea948a7b42
Rosa Naharro Vaillant <rosana50factoria@gmail.com>
2026-07-03 09:48:34 +0200

    fix:fields unique email, role

--- src/main/java/com/mundotech/newspaper/entity/Role.java
+++ src/main/java/com/mundotech/newspaper/entity/Role.java
@@ -19,7 +19,7 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id; 
 
-    @Column(nullable = false)
+    @Column(nullable = false,unique = true)
     private String role;
 
 }
