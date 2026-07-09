f476f7c8949c6efa376eb1ff633a25a35dff621b
rosana50factoria <rosana50factoria@gmail.com>
2026-07-09 08:50:10 +0200

    Pmt 37 ver todos los articulos y filtrar por (#13)
    * feat: get all articles and validation get all not empty
    
    * feat: get article by id

--- src/main/java/com/mundotech/newspaper/service/ArticleService.java
+++ src/main/java/com/mundotech/newspaper/service/ArticleService.java
@@ -1,7 +1,13 @@
 package com.mundotech.newspaper.service;
 
+import java.util.List;
+
 import com.mundotech.newspaper.entity.Article;
 
 public interface ArticleService {
     public Article createArticle(Article article, int userId);
+
+    public List<Article> getAllArticles();
+
+    public Article getArticleById(int id);
 }
