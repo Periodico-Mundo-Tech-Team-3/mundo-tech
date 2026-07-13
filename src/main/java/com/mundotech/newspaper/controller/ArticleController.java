d28eb1357c22e697885440d4795ca0f06f6a8df8
Rosa Naharro Vaillant <rosana50factoria@gmail.com>
2026-07-13 10:28:55 +0200

    feat: update article with validation exist article and owner article

--- src/main/java/com/mundotech/newspaper/controller/ArticleController.java
+++ src/main/java/com/mundotech/newspaper/controller/ArticleController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -59,6 +60,13 @@ public class ArticleController {
     List<ArticleInfoDto> responses =
             articleMapper.toArticleResponseList(articleService.getArticlesByUserId(authorId));
     return new ResponseEntity<>(responses, HttpStatus.OK);
-}
+    }
+
+    @PutMapping("/{id}/{userLoginId}")
+    public ResponseEntity<ArticleInfoDto> updateArticle(@PathVariable Integer id,@PathVariable Integer userLoginId, @Valid @RequestBody Article article){
+        ArticleInfoDto articleDto=articleMapper.toArticleInfoDto(articleService.updateArticle(id,userLoginId, article));
+
+        return new ResponseEntity<>(articleDto, HttpStatus.OK);
+    }
 
 }
