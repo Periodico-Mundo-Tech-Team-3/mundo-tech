110b67e8411c04621a961f50205da62c6f1f7b5f
Rosa Naharro Vaillant <rosana50factoria@gmail.com>
2026-07-15 10:41:32 +0200

    Merge con dev

--- src/main/java/com/mundotech/newspaper/controller/FileUploadController.java
+++ src/main/java/com/mundotech/newspaper/controller/FileUploadController.java
@@ -1,15 +1,8 @@
 package com.mundotech.newspaper.controller;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
